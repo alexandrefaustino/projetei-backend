5002b4d1f4edd448e0e5977272ef7239aea49b7d
alexandrefaustino <alexandrefaculdade@gmail.com>
2021-06-20 20:25:36 -0300

    alteração no arquivo config.js - correção de variável

--- config/config.js
+++ config/config.js
@@ -1,28 +1,26 @@
 require('dotenv/config');
 
 module.exports = {
-  {
-    "development": {
-      "username": process.env.MYSQL_USER,
-      "password": process.env.MYSQL_PASSWORD,
-      "database": process.env.MYSQL_PASSWORD,
-      "host": process.env.HOST_NAME,
-      "dialect": "mysql"
-    },
-    "test": {
-      "username": "root",
-      "password": null,
-      "database": "database_test",
-      "host": "127.0.0.1",
-      "dialect": "mysql"
-    },
-    "production": {
-      "username": process.env.MYSQL_USER_PRODUCTION,
-      "password": process.env.MYSQL_PASSWORD_PRODUCTION,
-      "database": process.env.MYSQL_PASSWORD_PRODUCTION,
-      "host": process.env.HOST_NAME_PRODUCTION,
-      "dialect": "mysql"
-    }
+  "development": {
+    "username": process.env.MYSQL_USER,
+    "password": process.env.MYSQL_PASSWORD,
+    "database": process.env.DATABASE,
+    "host": process.env.HOST_NAME,
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": process.env.MYSQL_USER_PRODUCTION,
+    "password": process.env.MYSQL_PASSWORD_PRODUCTION,
+    "database": process.env.DATABASE_PRODUCTION,
+    "host": process.env.HOST_NAME_PRODUCTION,
+    "dialect": "mysql"
   }
 }
 
