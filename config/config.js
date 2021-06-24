294c0be5e33bbe5d3126c0805fceb02100237181
alexandrefaustino <alexandrefaculdade@gmail.com>
2021-06-24 08:18:19 -0300

    importação do dotenv para o arquivo config

--- config/config.js
+++ config/config.js
@@ -1,7 +1,5 @@
 require('dotenv').config();
 
-console.log('usuario: ', process.env.MYSQL_USER_PRODUCTION);
-
 module.exports = {
   "development": {
     "username": process.env.MYSQL_USER_PRODUCTION,
