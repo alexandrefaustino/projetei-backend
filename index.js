3c5df776298ffeb70e06f21749c30437bc1f8257
alexandrefaustino <alexandrefaculdade@gmail.com>
2021-06-23 23:31:09 -0300

    coreção da url do heroku no index.js raiz

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const userRouter = require('./routers/userRouter');
-const URL = https://projetei-backend.herokuapp.com;
+const URL = 'https://projetei-backend.herokuapp.com';
 
 const app = express();
 app.use(express.json());
